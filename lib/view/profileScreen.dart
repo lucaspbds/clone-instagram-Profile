6221f1a6476b2c52f312ad11a64076c767b1ca2a
lucaspbds <118389176+lucaspbds@users.noreply.github.com>
2024-06-19 21:16:29 -0300

    Thirst commit

--- lib/view/profileScreen.dart
+++ lib/view/profileScreen.dart
@@ -6,7 +6,7 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 231, 245, 255),
+      backgroundColor: Color.fromARGB(255, 84, 84, 84),
       body: SafeArea(
         child: CustomScrollView(
           slivers: [
@@ -26,7 +26,7 @@ class ProfileScreen extends StatelessWidget {
                             height: 100.0,
                             decoration: BoxDecoration(
                               shape: BoxShape.circle,
-                              image: DecorationImage(image: AssetImage('assets/profilePhoto.jpeg')),
+                              image: DecorationImage(image: AssetImage('lib/assets/profilePhoto.jpeg')),
                               color: Colors.black
                             ),
                           )
@@ -45,7 +45,7 @@ class ProfileScreen extends StatelessWidget {
                                       style: TextStyle(
                                         fontWeight: FontWeight.bold,
                                         fontSize: 16,
-                                        color: Colors.black,
+                                        color: Color.fromARGB(255, 255, 255, 255),
                                       ),
                                     ),
                                   ),
@@ -57,7 +57,7 @@ class ProfileScreen extends StatelessWidget {
                                       style: TextStyle(
                                         fontWeight: FontWeight.bold,
                                         fontSize: 16,
-                                        color: Colors.black,
+                                        color: Color.fromARGB(255, 255, 255, 255),
                                       ),
                                     ),
                                   ),
@@ -68,7 +68,7 @@ class ProfileScreen extends StatelessWidget {
                                       style: TextStyle(
                                         fontWeight: FontWeight.bold,
                                         fontSize: 16,
-                                        color: Colors.black,
+                                        color: Color.fromARGB(255, 255, 255, 255),
                                       ),
                                     ),
                                   ),
@@ -81,21 +81,21 @@ class ProfileScreen extends StatelessWidget {
                                     "Publicações",
                                     style: TextStyle(
                                       fontSize: 13,
-                                      color: Colors.black,
+                                      color: Color.fromARGB(255, 255, 255, 255),
                                     ),
                                   ),
                                   Text(
                                     "Seguidores",
                                     style: TextStyle(
                                       fontSize: 13,
-                                      color: Colors.black,
+                                      color: Color.fromARGB(255, 255, 255, 255),
                                     ),
                                   ),
                                   Text(
                                     "Seguindo",
                                     style: TextStyle(
                                       fontSize: 13,
-                                      color: Colors.black,
+                                      color: Color.fromARGB(255, 255, 255, 255),
                                     ),
                                   ),
                                 ],
@@ -113,7 +113,7 @@ class ProfileScreen extends StatelessWidget {
                           child: const Text(
                             "Catholic Church",
                             style: TextStyle(
-                              color: Colors.black,
+                              color: Color.fromARGB(255, 255, 255, 255),
                               fontSize: 12,
                               fontWeight: FontWeight.bold,
                             ),
@@ -124,6 +124,7 @@ class ProfileScreen extends StatelessWidget {
                     SizedBox(height: 5),
                     SizedBox(
                       height: 30,
+                      width: 150,
                       child: TextButton(
                         style: ButtonStyle(
                           padding: MaterialStatePropertyAll(
@@ -138,36 +139,41 @@ class ProfileScreen extends StatelessWidget {
                               )
                             ),
                           ),
-                          foregroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(255, 255, 255, 255)),
+                          foregroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(255, 0, 0, 0)),
                           overlayColor: MaterialStateProperty.resolveWith<Color?>(
                             (Set<MaterialState> states) {
                               if (states.contains(MaterialState.hovered)) {
-                                return const Color.fromARGB(255, 131, 130, 130).withOpacity(0.04);
+                                return Color.fromARGB(255, 110, 110, 110).withOpacity(0.04);
                               }
                               if (states.contains(MaterialState.focused) || states.contains(MaterialState.pressed)) {
-                                return const Color.fromARGB(255, 131, 130, 130).withOpacity(0.12);
+                                return Color.fromARGB(255, 113, 113, 113).withOpacity(0.12);
                               }
                               return null;
                             },
                           ),
-                          backgroundColor: MaterialStateProperty.all<Color>(const Color.fromARGB(255, 165, 188, 210)),
+                          backgroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(255, 172, 212, 248)),
                         ),
                         onPressed: () {},
-                        child: const Text(
-                          "@ catholicChurch",
-                          style: TextStyle(
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.alternate_email
+                            ),
+                            Text("catholicChurch",
+                            style: TextStyle(
                             fontSize: 12,
-                            fontWeight: FontWeight.w300
-                          ),
-                        ),
+                            fontWeight: FontWeight.w300,
+                              )
+                            )
+                          ],
+                        )
                       ),
                     ),
-                    const SizedBox(height: 20),
                     Padding(
                       padding: EdgeInsetsDirectional.fromSTEB(0.0, 20.0, 0.0, 0.0),
                       child: Row(
                         mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        mainAxisAlignment: MainAxisAlignment.start,
                         children: [
                           SizedBox(
                             height: 35,
@@ -183,7 +189,7 @@ class ProfileScreen extends StatelessWidget {
                                   ),
                                 ),
                                 padding: MaterialStateProperty.all(const EdgeInsetsDirectional.fromSTEB(24.0, 0.0, 24.0, 0.0)),
-                                foregroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(255, 255, 255, 255)),
+                                foregroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(255, 0, 0, 0)),
                                 overlayColor: MaterialStateProperty.resolveWith<Color?>(
                                   (Set<MaterialState> states) {
                                     if (states.contains(MaterialState.hovered)) {
@@ -207,6 +213,7 @@ class ProfileScreen extends StatelessWidget {
                               ),
                             ),
                           ),
+                          SizedBox(width: 25,),
                           SizedBox(
                             height: 35,
                             child: TextButton(
@@ -221,7 +228,7 @@ class ProfileScreen extends StatelessWidget {
                                   ),
                                 ),
                                 padding: MaterialStateProperty.all(const EdgeInsetsDirectional.fromSTEB(24.0, 0.0, 24.0, 0.0)),
-                                foregroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(255, 255, 255, 255)),
+                                foregroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(255, 0, 0, 0)),
                                 overlayColor: MaterialStateProperty.resolveWith<Color?>(
                                   (Set<MaterialState> states) {
                                     if (states.contains(MaterialState.hovered)) {
@@ -245,8 +252,10 @@ class ProfileScreen extends StatelessWidget {
                               ),
                             ),
                           ),
+                          SizedBox(width: 25,),
                           SizedBox(
                             height: 35,
+                            width: 55,
                             child: TextButton(
                               style: ButtonStyle(
                                 shape: MaterialStateProperty.all<RoundedRectangleBorder>(
@@ -259,7 +268,7 @@ class ProfileScreen extends StatelessWidget {
                                   ),
                                 ),
                                 padding: MaterialStateProperty.all(const EdgeInsetsDirectional.fromSTEB(24.0, 0.0, 24.0, 0.0)),
-                                foregroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(255, 255, 255, 255)),
+                                foregroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(255, 0, 0, 0)),
                                 overlayColor: MaterialStateProperty.resolveWith<Color?>(
                                   (Set<MaterialState> states) {
                                     if (states.contains(MaterialState.hovered)) {
@@ -275,11 +284,11 @@ class ProfileScreen extends StatelessWidget {
                               ),
                               onPressed: () {},
                               child: SizedBox(
-                                height: 20,
-                                width: 20,
-                                child: ImageIcon(
-                                  AssetImage('assets/icons/contactAdd.png'), 
-                                  size: 5,
+                                height: 50,
+                                width: 700,
+                                child: Icon(
+                                  Icons.person_add,
+                                  size: 20,
                                 )
                               )
                             ),
@@ -288,12 +297,12 @@ class ProfileScreen extends StatelessWidget {
                       ),
                     ),
                     Padding(
-                      padding: EdgeInsetsDirectional.fromSTEB(0.0, 20.0, 0.0, 0.0),
+                      padding: EdgeInsetsDirectional.fromSTEB(8.0, 20.0, 0.0, 0.0),
                       child: Row(
                         mainAxisSize: MainAxisSize.max,
                         children: [
                           Padding(
-                            padding: EdgeInsetsDirectional.fromSTEB(8.0, 0.0, 0.0, 0.0),
+                            padding: EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
                             child: Container(
                               width: 100,
                               height: 100,
@@ -304,19 +313,102 @@ class ProfileScreen extends StatelessWidget {
                             ),
                           ),
                           Padding(
-                            padding: EdgeInsetsDirectional.fromSTEB(8.0, 0.0, 0.0, 0.0),
+                            padding: EdgeInsetsDirectional.fromSTEB(30.0, 0.0, 0.0, 0.0),
                             child: Container(
                               width: 100,
                               height: 100,
                               decoration: BoxDecoration(
                                 shape: BoxShape.circle,
-                                color: Colors.black,
+                                color: Colors.transparent,
+                                border: Border.all(color: Colors.white)
+
+                              ),
+                              child: Icon(
+                                Icons.add,
+                                color: Colors.white,
+                                size: 30,
                               ),
                             ),
                           )
                         ],
                       )
-                    )
+                    ),
+                  Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(30.0, 0.0, 0.0, 0.0),
+                    child: Row(
+                      children: [
+                        Text("Destaques",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 12,
+                            fontWeight: FontWeight.w300
+                          ),
+                        ),
+                        SizedBox(width: 88,),
+                        Text("Novo",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 12,
+                            fontWeight: FontWeight.w300
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 35, 0, 0),
+                    child: Row(
+                      children: [
+                        TextButton(
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStatePropertyAll(Colors.transparent),
+                          ),
+                          onPressed: () {}, 
+                          child: Icon(
+                            Icons.grid_on,
+                            color: Colors.white,
+                            size: 30,
+                            ),
+                          ),
+                          TextButton(
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStatePropertyAll(Colors.transparent),
+                          ),
+                          onPressed: () {}, 
+                          child: Icon(
+                            Icons.airplay,
+                            color: Colors.white,
+                            size: 30,
+                            ),
+                          ),
+                          TextButton(
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStatePropertyAll(Colors.transparent),
+                            
+                          ),
+                          onPressed: () {}, 
+                          child: Icon(
+                            Icons.person_pin,
+                            color: Colors.white,
+                            size: 30,
+                            ),
+                          )
+                      ],
+                    ), 
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(8.0, 0, 0, 0),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 80,
+                          height: 80,
+                          child: Image(image: AssetImage("lib/assets/post01.jpeg")),
+                        )
+                      ],
+                    ),
+                    ),
+    
                   ],
                 ),
               ),
